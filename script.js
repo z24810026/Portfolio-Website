fc5ec6ebceff5a46eed158b06007857205627b87
MinamiHuang <minami@minamideMacBook-Air.local>
2025-07-06 14:28:17 +0900

    Portfolio Website 初始版本

---
+++ script.js
@@ -0,0 +1,8 @@
+var types = new Typed(".multiple-text",{
+    strings:["フロントエンジニア","バックエンドエンジニア",
+    "Webデザイナーエンジニア"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+})
